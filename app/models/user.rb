cbb7894ab721d20e6bec73b2ca52e5b876d2d057
Jeffrey Weng <jweng8@binghamton.edu>
2021-04-20 21:32:09 +0000

    part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :authorizations
+  has_many :authorizations, :dependent => :destroy
   validates :name, :email, :presence => true
+    #create new user
+  validate :staff_or_student
   
+  def staff_or_student
+    errors.add(:email, "must have SUNY ID") if
+      email.split('@')[1] != "binghamton.edu"
+  end
+  def self.create_with_omniauth info
+    create! do |user|
+      user.email = info["email"]
+      user.name = info["name"]
+    end
+  end
 end
